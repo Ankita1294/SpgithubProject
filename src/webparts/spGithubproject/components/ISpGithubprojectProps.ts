118030e5fde2a8ba31ef4f2e8855f8e6b19260aa
Ankita1294 <ankita1294@outlook.com>
2021-07-03 22:32:26 +0530

    initial commit fixes AB#95

---
+++ src/webparts/spGithubproject/components/ISpGithubprojectProps.ts
@@ -0,0 +1,3 @@
+export interface ISpGithubprojectProps {
+  description: string;
+}
---
+++ src/webparts/spGithubproject/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface ISpGithubprojectWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'SpGithubprojectWebPartStrings' {
+  const strings: ISpGithubprojectWebPartStrings;
+  export = strings;
+}
