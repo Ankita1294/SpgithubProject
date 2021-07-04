3c9daa33b1fdbc5df76a09c4f9f71ba980b5e368
Ankita1294 <ankitatripathy1294@gmail.com>
2021-07-04 14:04:03 +0530

    text changed

--- src/webparts/spGithubproject/components/SpGithubproject.tsx
+++ src/webparts/spGithubproject/components/SpGithubproject.tsx
@@ -10,7 +10,7 @@ export default class SpGithubproject extends React.Component<ISpGithubprojectPro
         <div className={ styles.container }>
           <div className={ styles.row }>
             <div className={ styles.column }>
-              <span className={ styles.title }>Welcome to SharePoint!</span>
+              <span className={ styles.title }>Managing Sharepoint Framework using Github and Azure DevOps!</span>
               <p className={ styles.subTitle }>Customize SharePoint experiences using Web Parts.</p>
               <p className={ styles.description }>{escape(this.props.description)}</p>
               <a href="https://aka.ms/spfx" className={ styles.button }>
